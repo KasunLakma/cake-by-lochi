0d59211cb9ef3b8e14ebd58165d9fe660873057f
Kasun Lakmal <kasunlakmal20487ks1@gmail.com>
2026-07-09 06:34:52 +0530

    refactor: resolve connection issues by linking Prisma schema datasource to environment variables

--- prisma.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@prisma/config'
-
-export default defineConfig({
-  datasource: {
-    url: "postgresql://neondb_owner:npg_vndvBNt1DONg@ep-lingering-salad-a5ipt27d-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-  },
-})
